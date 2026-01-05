e7d484fae72b4878de1fc6c1523167dfc01c3afa
janreidev-web <janreidev@gmail.com>
2026-01-06 00:58:43 +0800

    Fix pydantic-settings version error

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,8 @@ Configuration settings for FMCG Data Analytics Platform
 
 import os
 from typing import Optional
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
